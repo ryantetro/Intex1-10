7da5540eb8a23d43735679a6bae1f6184c7f923a
alexandrapesantez <103144953+alexandrapesantez@users.noreply.github.com>
2024-04-09 12:25:25 -0600

    changes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 using Microsoft.AspNetCore.Identity;
+=======
+using Intex1_10.Models;
+using Microsoft.EntityFrameworkCore;
+>>>>>>> 93eff35111292159783241a3466e07f75d8c39cb
 
 using Intex1_10.Models;
 using Microsoft.EntityFrameworkCore;
@@ -8,6 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+<<<<<<< HEAD
 builder.Services.Configure<IdentityOptions>(options =>
 {
     // Default Password settings.
@@ -24,6 +30,14 @@ builder.Services.Configure<CookiePolicyOptions>(options =>
     options.CheckConsentNeeded = context => true;
     options.MinimumSameSitePolicy = SameSiteMode.None;
 });
+=======
+builder.Services.AddDbContext<IntexDatabaseContext>(options =>
+{
+    options.UseSqlite(builder.Configuration["ConnectionStrings:IntexConnection"]);
+});
+
+builder.Services.AddScoped<IIntexRepository, EFIntexRepository>(); 
+>>>>>>> 93eff35111292159783241a3466e07f75d8c39cb
 
 builder.Services.AddDbContext<IntexDatabaseContext>(options =>
 {
