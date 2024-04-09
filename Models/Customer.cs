44ca76167d179ed2bf90b65648424f93abc209fc
ryantetro <Ryantetro@gmail.com>
2024-04-09 09:46:54 -0600

    Built the Models

---
+++ Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Intex1_10.Models;
+
+public partial class Customer
+{
+    [Key] // This data annotation denotes that CustomerId is the primary key.
+    public int CustomerId { get; set; }
+
+    [Required] // Makes sure FirstName cannot be null in the database.
+    public string FirstName { get; set; }
+
+    [Required] // Makes sure LastName cannot be null in the database.
+    public string LastName { get; set; }
+    
+    public DateOnly BirthDate { get; set; } // Assuming you want to allow nulls for BirthDate.
+    public string CountryOfResidence { get; set; }
+    public string Gender { get; set; }
+
+    // If Age is a computed value, it's often not stored in the database
+    // but calculated based on BirthDate. If you choose to store it, you might want it to be non-nullable.
+    public int? Age { get; set; } 
+
+    [EmailAddress] // This will enforce that the Email property, if present, contains a valid email address.
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+}
---
+++ Models/LineItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Intex1_10.Models;
+
+public partial class LineItem
+{
+    [Key] // This data annotation denotes that TransactionId and ProductId together form the primary key.
+    public string TransactionId { get; set; }
+
+    [Key]
+    public string ProductId { get; set; }
+
+    public int Qty { get; set; } // Assuming Qty is always an integer value, so removed the nullable indicator.
+
+    public int? Rating { get; set; } // Assuming Rating can be nullable.
+    
+}
